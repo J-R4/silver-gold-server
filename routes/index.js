734befc742512e003e8ca751b711d55ec05bf488
J-R4 <JoshuaFanera@gmail.com>
2021-03-16 00:31:21 +0700

    revise in the routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,30 @@
 const router = require('express').Router();
-
 const UserController = require('../controllers/UserController.js');
 const ProductController = require('../controllers/ProductController.js');
 
-router.get('/login', UserController.login);
+const { authenticate, authorize } = require('../middlewares/auth.js');
+
+router.get('/', (req, res) => {
+    res.send('welcome to silver&gold good people !');
+});
+
+//User Controller
+router.post('/login', UserController.login);
+router.post('/oAuth', UserController.loginGoogle);
+
+router.post('/register', UserController.register);
+
+router.use(authenticate);
+
+//ProductController
+router.get('/products', ProductController.products);
+router.post('/products', ProductController.postProduct);
+
+router.use(authorize);
+
+router.get('/products/:id', ProductController.getOne);
+router.put('/products/:id', ProductController.put);
+router.patch('/products/:id', ProductController.patch);
+router.delete('/products/:id', ProductController.delete);
 
 module.exports = router;
