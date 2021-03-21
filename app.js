b4ac5a983f1b726e3188510da62ff67a88651fef
J-R4 <JoshuaFanera@gmail.com>
2021-03-21 16:45:07 +0700

    ready to heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const port = process.env.PORT || 3000;
 
 const router = require('./routes/index.js');
 
@@ -14,5 +15,7 @@ app.use(express.json());
 app.use('/', router);
 
 app.use(errHandler);
+
+app.listen(port, () => console.log(`S&G admin app is listening on port http://localhost:`+port));
 
 module.exports = app;
